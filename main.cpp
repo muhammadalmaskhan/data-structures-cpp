30a8c98604fb28ad1d9148755db3b8ffcf9ec257
Muhammad Almas Khan <mkhan@cs.qau.edu.pk>
2025-05-25 14:05:01 +0500

    Initial commit: testing clion integerations with github

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main() {
     cout<<"Just a testing code from where i will test each data structure";
     cout<<endl;
+    for (int i = 0; i < 10; i++) {
+        cout<<i<<endl;
+    }
 
     return 0;
 }
